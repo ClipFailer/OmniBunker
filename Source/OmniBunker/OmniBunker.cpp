8b608e1acd1ada2c6cf6ae7e098cb538b8f7eb5b
clipfail <rogroty@gmail.com>
2026-07-17 20:05:39 +0400

    Init

---
+++ Source/OmniBunker/OmniBunker.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "OmniBunker.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, OmniBunker, "OmniBunker" );
